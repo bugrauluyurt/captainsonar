e62a0c196100a0a73af1cc0fd82bd9a51c2b73bd
Bugra Uluyurt <bugrauluyurt@microsoft.com>
2024-02-19 19:29:14 -0500

    mine detonation logic completed

---
+++ Assets/Mine.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CaptainSonar.Map;
+using CaptainSonar;
+
+namespace CaptainSonar.Assets
+{
+    internal class Mine
+    {
+        private Coordinate _location;
+        private CaptainSonar.Player.Player _owner;
+        private bool _isActive = false;
+
+        public Mine(CaptainSonar.Player.Player owner, Coordinate location, bool shouldActivateOnInit = true)
+        {
+            _location = location;
+            _owner = owner;
+            if (shouldActivateOnInit)
+            {
+                Activate();
+            }
+        }
+
+        public Coordinate GetLocation() => _location;
+        public CaptainSonar.Player.Player GetOwner() => _owner;
+
+        private bool CanDetonate(CaptainSonar.Player.Player player)
+        {
+            return _isActive && _owner == player;
+        }
+
+        public void Activate()
+        {
+            _isActive = true;
+        }
+
+        public bool IsActivated()
+        {
+            return _isActive;
+        }
+
+        public bool Detonate(CaptainSonar.Player.Player player)
+        {
+            if (!CanDetonate(player)) {
+                Console.WriteLine("The player cannot detonate this mine");
+                return false;
+            }
+            Console.WriteLine($"The mine located at {_location.X}:{_location.Y} has been detonated by {player.Name}");
+            return true;
+        }
+    }
+}
---
+++ Helpers.cs
@@ -0,0 +1,19 @@
+﻿using CaptainSonar.Map;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptainSonar
+{
+    internal class Helpers
+    {
+        static public int CalculateShortestDistance(Coordinate location1, Coordinate location2)
+        {
+            // @TODO: Calculate the shortes distance between two coordinates here while avoiding obstacles
+            // @TODO: First create the maps and write tests after you implement the algorithm
+            return 1;
+        }
+    }
+}
